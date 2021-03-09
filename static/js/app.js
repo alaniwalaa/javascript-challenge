90cbb48e5f18e1855b5bc8ad1dbab6739a140267
walaa <w.alani2010@yahoo.com>
2021-03-08 18:03:28 -0800

    the filter according to date

--- static/js/app.js
+++ static/js/app.js
@@ -4,34 +4,29 @@ var tableData = data;
 // THE CODE 
 //  ##### UFO -LEVEL-1 ##### // 
 
-// Append a table to the web page 
-var table = d3.select('table')
-
-// table outlook 
-table.attr('class', 'table table-striped');
-
 // select table body
 var tbody = d3.select('tbody');
 
-// append table row to table body     
-//### And make the comparison to your input values using the filter()function  
-tableData.forEach((data) => {
-    var row = tbody.append('tr');
-    Object.entries(data).forEach(([key, value]) => {
-        var cell = row.append('td');
-        cell.text(value);
+function buildTable(data) {
+    tbody.html('');
+
+    // append table row to table body     
+    //### And make the comparison to your input values using the filter()function  
+    data.forEach((rowData) => {
+        var row = tbody.append('tr');
+        Object.values(rowData).forEach((value) => {
+            let cell = row.append('td');
+            cell.text(value);
+        });
     });
-});
+};
 
 // search columns to match user input - listen for events 
 // reference the button 
 var button = d3.select('#filter-btn');
 
 // reference the input field 
-var userInput = d3.select('#form-control');
-
-// create event handlers
-button.on('click', handleClick);
+var userInput = d3.select('.form-control');
 
 // click function 
 function handleClick() {
@@ -39,22 +34,14 @@ function handleClick() {
     // prevent the page from refreshing 
     d3.event.preventDefault();
 
-    // select the input element and get the raw HTML node
-    var inputElement = d3.select('.form-control');
+    var date = d3.select('#datetime').property('value'); 
+    let filteredData = tableData; 
 
-    // get the value property of the input element
-    var inputValue = inputElement.property('value')
-    
-    console.log(inputValue);
-    console.log(tableData);
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+        };
+    buildTable(filteredData);
 }
 
-
-// input field trigger change event - new text entered 
-userInput.on('change', function() {
-    var newInput = d3.event.target.value;
-    console.log(newInput);
-});
-
-
-//  ##### UFO -LEVEL-2 ##### // 
+d3.selectAll('#filter-btn').on('click', handleClick);
+buildTable(tableData);
